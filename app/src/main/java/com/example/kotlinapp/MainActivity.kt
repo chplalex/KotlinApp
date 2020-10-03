2d808836dde9dff4704526e6afc96ea35c28927c
Alexander Chepel <chepel.alexander@gmail.com>
2020-10-03 11:30:40 +0300

    Добавил кнопку выключения. Добавил реакцию на нажатие основной кнопки.

--- app/src/main/java/com/example/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlinapp
 import android.app.Activity
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,13 +17,21 @@ class MainActivity : AppCompatActivity() {
 
     fun btnOnClick(view: View) {
         txtView.text = "!!! вернусь через 2 секунды !!!"
+        btnView.visibility = GONE
         val activity: Activity = this
         val thread = Thread(Runnable {
             Thread.sleep(2000L)
-            activity.runOnUiThread(Runnable { txtView.text = "Hello world!" })
+            activity.runOnUiThread(Runnable {
+                txtView.text = "Hello world!"
+                btnView.visibility = VISIBLE
+            })
         })
         thread.isDaemon = true
         thread.start()
+    }
+
+    fun imgOnClick(view: View) {
+        finish()
     }
 
 }
